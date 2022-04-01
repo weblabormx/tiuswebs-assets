abf396167ce1396fa43af6f208e091ad4e6bc29a
Luan Himmlisch <luisangelserranocatala@gmail.com>
2022-03-31 22:13:59 -0600

    Fixed mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,13 @@ let mix = require('laravel-mix');
 require('mix-tailwindcss');
 
 mix
-    .scripts([
-        '/node_modules/jquery/dist/jquery.min.js',
-        '/node_modules/bootstrap/dist/js/bootstrap.min.js',
-        '/src/js/web.js'
-    ], '/dist/js/web.js')
-    .sass('/src/sass/web.scss', '/dist/css/')
+    .setPublicPath('dist')
+    .js([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'src/js/web.js'
+    ], 'js/web.js')
+    .sass('src/sass/web.scss', 'css/')
     .tailwind()
+    .sourceMaps()
     .version()
